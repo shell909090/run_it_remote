febc1d2749d1dd20660fefb4bd4df50096f76014
Shell.Xu <shell909090@gmail.com>
2015-08-15 09:27:09 -0700

    * add network.

--- core.py
+++ core.py
@@ -1,17 +1,4 @@
-import os, sys, imp, zlib, StringIO, tempfile
-
-stdout = os.fdopen(os.dup(1), 'w')
-os.close(1)
-os.dup2(2, 1)
-
-def write(o):
-    marshal.dump(o, stdout)
-    stdout.flush()
-
-class StdPipe(object):
-    def write(self, s): write(['otpt', s])
-    def flush(self): write(['flsh', s])
-sys.stdout = StdPipe()
+import os, sys, imp, zlib, getopt, struct, marshal, StringIO, tempfile
 
 def add_module(name):
     if name not in sys.modules:
@@ -22,33 +9,32 @@ class Loader(object):
     def __init__(self, src, pathname, description):
         self.src, self.pathname, self.description = src, pathname, description
     def exec_code_module(self, mod):
-        co = compile(zlib.decompress(self.src), self.pathname, 'exec')
+        co = compile(self.src, self.pathname, 'exec')
         exec co in mod.__dict__
 
 class SrcLoader(Loader):
     def load_module(self, fullname):
-        mod = add_module(fullname)
-        mod.__file__ = self.pathname
-        self.exec_code_module(mod)
-        return mod
+        m = add_module(fullname)
+        m.__file__ = self.pathname
+        self.exec_code_module(m)
+        return m
 
 class ExtLoader(Loader):
     def load_module(self, fullname):
         with tempfile.NamedTemporaryFile('wb') as tmp:
-            tmp.write(zlib.decompress(self.src))
+            tmp.write(self.src)
             tmp.flush()
-            imp.load_dynamic(fullname, tmp.name)
-        return mod
+            return imp.load_dynamic(fullname, tmp.name)
 
 class PkgLoader(Loader):
     def load_module(self, fullname):
         loader = finder.find_remote('__init__', [self.pathname,])
-        mod = add_module(fullname)
-        mod.__file__ = loader.pathname
-        mod.__path__ = [self.pathname,]
-        mod.__package__ = fullname
-        loader.exec_code_module(mod)
-        return mod
+        m = add_module(fullname)
+        m.__file__ = loader.pathname
+        m.__path__ = [self.pathname,]
+        m.__package__ = fullname
+        loader.exec_code_module(m)
+        return m
 
 class Finder(object):
     def find_module(self, name, path):
@@ -60,7 +46,7 @@ class Finder(object):
     def find_remote(self, name, path):
         # print 'find remote:', name, path
         write(['imp', name.split('.')[-1], path])
-        r = marshal.load(sys.stdin)
+        r = read()
         if r is not None: return self.type_map[r[2][2]](*r)
     type_map = {
         imp.PY_SOURCE: SrcLoader,
@@ -70,10 +56,67 @@ class Finder(object):
 finder = Finder()
 sys.meta_path.append(finder)
 
-glb = dict()
-while True:
-    o = marshal.load(sys.stdin)
-    if o[0] == 'exit': break
-    if o[0] == 'exec': co = compile(o[1], '<exec>', 'exec')
-    elif o[0] == 'eval': co = compile(o[1], '<eval>', 'eval')
-    write(['rslt', eval(co, glb)])
+class StdPipe(object):
+    def write(self, s): write(['otpt', s])
+    def flush(self): write(['flsh', s])
+sys.stdout = StdPipe()
+
+def loop():
+    g = dict()
+    while True:
+        o = read()
+        if o[0] == 'exit': break
+        if o[0] == 'exec': co = compile(o[1], '<exec>', 'exec')
+        elif o[0] == 'eval': co = compile(o[1], '<eval>', 'eval')
+        elif o[0] == 'aply':
+            pass
+        write(['rslt', eval(co, g)])
+
+def main_exec():
+    stdout = os.fdopen(os.dup(1), 'w')
+    os.close(1)
+    os.dup2(2, 1)
+    global write
+    def write(o):
+        d = zlib.compress(marshal.dumps(o))
+        stdout.write(struct.pack('>I', len(d)) + d)
+        stdout.flush()
+    global read
+    def read():
+        l = struct.unpack('>I', sys.stdin.read(4))[0]
+        return marshal.loads(zlib.decompress(sys.stdin.read(l)))
+    return loop()
+
+def main_net(host, port):
+    global write
+    global read
+    import socket
+    l = socket.socket()
+    l.bind((host, port))
+    l.listen(1)
+    while True:
+        s, a = l.accept()
+        f = s.makefile('rw')
+        def write(o):
+            d = zlib.compress(marshal.dumps(o))
+            f.write(struct.pack('>I', len(d)) + d)
+            f.flush()
+        def read():
+            l = struct.unpack('>I', f.read(4))[0]
+            return marshal.loads(zlib.decompress(f.read(l)))
+        loop()
+
+def main():
+    optlist, args = getopt.getopt(sys.argv[1:], 'hn:')
+    optdict = dict(optlist)
+    if '-h' in optdict:
+        print main.__doc__
+        return
+
+    if '-n' in optdict:
+        host, port = optdict['-n'].rsplit(':', 1)
+        port = int(port)
+        main_net(host, port)
+    else: main_exec()
+
+if __name__ == '__main__': main()
--- main.py
+++ main.py
@@ -4,36 +4,12 @@
 @date: 2015-08-14
 @author: shell.xu
 '''
-import os, sys, imp, zlib, inspect, marshal, subprocess
+import os, sys, imp, zlib, getopt, socket, struct, pprint
+import inspect, marshal, subprocess
 
-BOOTSTRAP = '''import sys, marshal; exec compile(marshal.load(sys.stdin), '<core>', 'exec')'''
+BOOTSTRAP = '''import sys, zlib, marshal; exec compile(zlib.decompress(marshal.load(sys.stdin)), '<core>', 'exec')'''
 
-class LocalHost(object):
-
-    def __init__(self):
-        self.p = subprocess.Popen(
-            ['python', '-c', BOOTSTRAP],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        with open('core.py', 'r') as fi:
-            self.write(fi.read())
-
-    def close(self):
-        self.write(['exit',])
-        self.p.wait()
-
-    def write(self, o):
-        marshal.dump(o, self.p.stdin)
-        self.p.stdin.flush()
-
-    def read(self):
-        try:
-            return marshal.load(self.p.stdout)
-        except:
-            print self.p.stderr.read()
-            raise
-
+class BaseInstance(object):
     def loop(self):
         while True:
             r = self.read()
@@ -42,53 +18,124 @@ class LocalHost(object):
             elif r[0] == 'flsh':
                 sys.stdout.flush()
             elif r[0] == 'imp':
-                try:
-                    # print 'local find:', r
-                    r = list(imp.find_module(r[1], r[2]))
-                    # print 'local result:', r
-                    if r[2][2] in (imp.PY_SOURCE, imp.C_EXTENSION):
-                        with r[0]: r[0] = zlib.compress(r[0].read(), 9)
-                    self.write(r)
-                except ImportError: self.write(None)
+                self.find_module(r[1], r[2])
             elif r[0] == 'rslt':
                 return r[1]
             elif r[0] == 'excp':
                 raise Exception(r[1])
 
-    def eval(self, f):
+    def find_module(self, name, path):
+        try:
+            r = list(imp.find_module(name, path))
+            if r[2][2] in (imp.PY_SOURCE, imp.C_EXTENSION):
+                with r[0]: r[0] = r[0].read()
+            self.write(r)
+        except ImportError: self.write(None)
+
+    @staticmethod
+    def check_f(f):
         if hasattr(f, '__call__'):
             f = inspect.getsource(f)
         assert isinstance(f, basestring)
-        self.write(['eval', f])
+        return f
+
+    def eval(self, f):
+        self.write(['eval', self.check_f(f)])
         return self.loop()
 
     def execute(self, f):
-        if hasattr(f, '__call__'):
-            f = inspect.getsource(f)
-        assert isinstance(f, basestring)
-        self.write(['exec', f])
+        self.write(['exec', self.check_f(f)])
         return self.loop()
 
-class RemoteHost(LocalHost):
+    def call(self, f, *p):
+        self.execute(f)
+        return self.eval('%s(%s)' % (f.__name__, str(p)[1:-1]))
 
-    def __init__(self, host):
+class ProcessInstance(BaseInstance):
+    def start(self, cmd):
         self.p = subprocess.Popen(
-            ['ssh', host, 'python', '-c', '"%s"' % BOOTSTRAP],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+            cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         with open('core.py', 'r') as fi:
-            self.write(fi.read())
+            marshal.dump(zlib.compress(fi.read(), 9), self.p.stdin)
+            self.p.stdin.flush()
+
+    def close(self):
+        self.write(['exit',])
+        self.p.wait()
+
+    def write(self, o):
+        d = zlib.compress(marshal.dumps(o), 9)
+        self.p.stdin.write(struct.pack('>I', len(d)) + d)
+        self.p.stdin.flush()
+
+    def read(self):
+        try:
+            l = struct.unpack('>I', self.p.stdout.read(4))[0]
+            return marshal.loads(zlib.decompress(self.p.stdout.read(l)))
+        except:
+            print self.p.stderr.read()
+            raise
+
+class LocalInstance(ProcessInstance):
+    def __init__(self):
+        self.start(['python', '-c', BOOTSTRAP])
+
+    def __repr__(self): return '<local>'
+
+class RemoteInstance(ProcessInstance):
+    def __init__(self, host):
+        self.host = host
+        self.start(['ssh', host, 'python', '-c', '"%s"' % BOOTSTRAP])
+
+    def __repr__(self): return self.host
+
+class NetInstance(BaseInstance):
+    def __init__(self, host, port):
+        self.s = socket.socket()
+        self.s.connect((host, port))
+        self.stdin = self.stdout = self.s.makefile('rw')
+        self.host = host
+
+    def __repr__(self): return self.host
+
+    def close(self):
+        self.write(['exit',])
+        self.stdin.close()
+
+    def write(self, o):
+        d = zlib.compress(marshal.dumps(o), 9)
+        self.stdin.write(struct.pack('>I', len(d)) + d)
+        self.stdin.flush()
+
+    def read(self):
+        l = struct.unpack('>I', self.stdout.read(4))[0]
+        return marshal.loads(zlib.decompress(self.stdout.read(l)))
 
 def main():
-    funcname = sys.argv[2]
-    modname = funcname.rsplit('.', 1)[0]
-    h = RemoteHost(sys.argv[1])
-    h.execute('import ' + modname)
-    r = h.eval(funcname + '()')
-    h.close()
-    print '----------output-----------'
-    import pprint
-    pprint.pprint(r)
+    optlist, args = getopt.getopt(sys.argv[1:], 'hn:m:')
+    optdict = dict(optlist)
+    if '-h' in optdict:
+        print main.__doc__
+        return
+
+    ilist = []
+    if '-n' in optdict:
+        for addr in optdict['-n'].split(','):
+            host, port = addr.rsplit(':', 1)
+            port = int(port)
+            ilist.append(NetInstance(host, port))
+    else:
+        for machine in optdict['-m'].split(','):
+            ilist.append(RemoteInstance(machine))
+
+    for funcname in args:
+        modname = funcname.rsplit('.', 1)[0]
+        for i in ilist:
+            i.execute('import ' + modname)
+            r = i.eval(funcname + '()')
+            print '----------%s output: %s-----------' % (str(i), funcname)
+            pprint.pprint(r)
+
+    for i in ilist: i.close()
 
 if __name__ == '__main__': main()
